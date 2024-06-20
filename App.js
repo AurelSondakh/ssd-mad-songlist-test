36a52f760667163ae10ef137900116f76ee31a98
aurelsondakh <aurelsondakh@gmail.com>
2024-06-20 21:29:09 +0800

    first push

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, React Native!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default App;
